813a39d3add4554533a0e35dbf1f61616f6b25cc
dinbandhu12 <dinbandhu8445@gmail.com>
2023-11-08 21:36:28 +0530

    front-end file added

---
+++ front_end/src/components/Layout.js
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+import React from "react";
+
+const Layout = () => {
+  return (
+    <maim>
+      <Outlet />
+    </maim>
+  )
+}
+
+export default Layout
